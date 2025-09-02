df99bff404a64e011c41c5675526e2d52ab3ea35
Devesh Kumar Patel <69602548+dev3z@users.noreply.github.com>
2025-09-02 18:08:05 +0530

    Time: 558 ms (20.06%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+        select p.project_id,
+        round(AVG(experience_years),2) as average_years 
+        from Project  p join Employee e on 
+        p.employee_id  = e.employee_id 
+        group by project_id  
+        order by  project_id  
+
